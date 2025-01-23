0634918041925b60b440e132c62586c89c5c82a6
Green PC <83514730+xacq@users.noreply.github.com>
2025-01-23 11:15:34 -0500

    Update 2025_01_22_220833_add_phone_number_to_users_table.php

--- proy1/database/migrations/2025_01_22_220833_add_phone_number_to_users_table.php
+++ proy1/database/migrations/2025_01_22_220833_add_phone_number_to_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    {    //SE DEFINE EL CAMPO CON LA CONFIGURACION SOLICITADA
         Schema::table('users', function (Blueprint $table) {
             $table->string('phone_number', 15)->unique()->nullable(false);
         });
@@ -20,7 +20,7 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
-    {
+    {    //SE GENERA LA CREACION DE LA NUEVA COLUMNA CON EL CAMPO CONFIGURADO
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('phone_number');
         });
