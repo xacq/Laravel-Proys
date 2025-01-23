2f60b4f6d79785f0735d58e84f07de750bb1b4ae
Green PC <83514730+xacq@users.noreply.github.com>
2025-01-23 11:17:20 -0500

    Update Controller.php

--- proy1/app/Http/Controllers/Controller.php
+++ proy1/app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function store(Request $request)
-    {
+    {    //VALIDO EL DATO QUE SE INGRESA COMO NUMERO TELEFONICO EN EL CONTROLADOR EN LA ACCION DE CREACION
         $validatedData = $request->validate([
             'phone_number' => ['required', 'string', 'max:15', 'regex:/^[0-9]+$/', 'unique:users,phone_number'],
         ]);
@@ -18,7 +18,7 @@ class UserController extends Controller
     }
 
     public function update(Request $request, User $user)
-    {
+    {    //VALIDO EL DATO DE NUMERO TELEFONICO CUANDO SE HACE UNA ACTUALIZACION DEL CAMPO
         $validatedData = $request->validate([
             'phone_number' => ['required', 'string', 'max:15', 'regex:/^[0-9]+$/', 'unique:users,phone_number,' . $user->id],
         ]);
