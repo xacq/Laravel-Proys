9e3432a0d9049ccbf1af1913ad5a4fa9fea65365
Green PC <83514730+xacq@users.noreply.github.com>
2025-01-23 11:38:30 -0500

    Update 2025_01_23_040352_create_products_table.php

--- proy2/database/migrations/2025_01_23_040352_create_products_table.php
+++ proy2/database/migrations/2025_01_23_040352_create_products_table.php
@@ -10,13 +10,13 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    {    //SE AUMENTO LOS CAMPOS A PARTE DE ID Y TIMESTAMP QUE YA SE GENEREAN AUTOMATICAMENTE
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 10, 2);
-            $table->integer('stock');
+            $table->string('name'); //NOMBRE DEL PRODUCTO
+            $table->text('description')->nullable(); //TEXTO DESCRIPTIVO DEL PRODUCTO
+            $table->decimal('price', 10, 2); //PRECIO DEL PRODUCTO
+            $table->integer('stock'); //STOCK DEL PRODUCTO
             $table->timestamps();
         });
     }
